c125479abe76843bb68b5c50558b779fb5ce9478
David Lang <davidlang42@gmail.com>
2025-02-27 22:26:39 +1100

    Nicer format last updated

--- backend/Requests.js
+++ backend/Requests.js
@@ -73,7 +73,7 @@ function listBalances(sheet_id, _sheet_already_open) {
     }
   }
   return {
-    last_updated: new Date().toString(),
+    last_updated: new Date().toLocaleString(Session.getActiveUserLocale()),
     balances: result
   };
 }
