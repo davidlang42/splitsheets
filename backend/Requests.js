a0ad34a34f57440907921bf79f73f462a43dc861
David Lang <davidlang42@gmail.com>
2025-02-27 22:29:54 +1100

    Take 3

--- backend/Requests.js
+++ backend/Requests.js
@@ -73,7 +73,7 @@ function listBalances(sheet_id, _sheet_already_open) {
     }
   }
   return {
-    last_updated: Utilities.formatDate(new Date(), Session.getScriptTimeZone(), 'yyyy-MM-dd\'T\'HH:mm:ss\'Z\''),
+    last_updated: Utilities.formatDate(new Date(), Session.getScriptTimeZone(), 'ddd dd MMM HH:mm:ss'),
     balances: result
   };
 }
