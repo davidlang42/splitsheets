f60f7fe8968bf0dae0e41f3ccc3643fb535c0a87
davidlang42 <37281992+davidlang42@users.noreply.github.com>
2025-02-28 00:45:46 +1100

    Issue2: Use names instead of email addresses (#25)
    * Variable rename only
    
    * Use aliases instead of emails for balances
    
    * alias instead of email - POSSIBLE LOST CHANGES
    
    * Revert "alias instead of email - POSSIBLE LOST CHANGES"
    
    This reverts commit abfc52ae08950454cc953f836bb8711da1ee2764.
    
    * alias instead of email - local changes
    
    * alias instead of email - POSSIBLE LOST CHANGES
    
    * Revert "alias instead of email - POSSIBLE LOST CHANGES"
    
    This reverts commit 41ac9576f97baf98e4ded59096a43f28bfe60db9.
    
    * Use aliases in notifications
    
    * alias in notifications - POSSIBLE LOST CHANGES
    
    * Revert "alias in notifications - POSSIBLE LOST CHANGES"
    
    This reverts commit 7aa34cf33d71180af3a8dd7f96e1c9b133fcb3fa.
    
    * Fix typo
    
    * fix typo - POSSIBLE LOST CHANGES
    
    * Revert "fix typo - POSSIBLE LOST CHANGES"
    
    This reverts commit d20e358c3882af6bf298197d047a82c0a4a41c78.

--- backend/Requests.js
+++ backend/Requests.js
@@ -37,7 +37,7 @@ function createSheet(name) {
   return listSheets();
 }
 
-// append a new cost row to the given sheet, then return the balances as {email: owed}
+// append a new cost row to the given sheet, then return the balances as {alias: owed}
 function addCost(sheet_id, date, description, amount, paid_by, paid_for, split) {
   var sheet = openSheet(sheet_id);
   var costs = sheet.getSheetByName(COSTS_SHEET);
@@ -59,12 +59,12 @@ function addCost(sheet_id, date, description, amount, paid_by, paid_for, split) 
   for (const email of paid_for_split) {
     if (email != my_email && !notify_emails.includes(email)) notify_emails.push(email);
   }
-  var p = PropertiesService.getUserProperties();
   var sheet_name = sheet.getName();
-  let body = "<p>" + my_email + " added a $" + amount + " cost called '" + description + "' to the '" + sheet_name + "' SplitSheet.<p>";
-  body += "<p>It was paid by " + paid_by + " for:</p><ul>";
+  var users = listUsers(sheet_id);
+  let body = "<p>" + (users[my_email] ?? my_email) + " added a $" + amount + " cost called '" + description + "' to the '" + sheet_name + "' SplitSheet.<p>";
+  body += "<p>It was paid by " + (users[paid_by] ?? paid_by) + " for:</p><ul>";
   for (const email of paid_for_split) {
-    body += "<li>" + email + "</li>";
+    body += "<li>" + (users[email] ?? email) + "</li>";
   }
   body += "</ul>";
   if (split) body += "<p>Split in the ratios: " + split + "</p>";
@@ -80,7 +80,7 @@ function addCost(sheet_id, date, description, amount, paid_by, paid_for, split) 
   return listBalances(sheet_id, sheet);
 }
 
-// return balances from a given sheet as {email: owed}
+// return balances from a given sheet as {alias: owed}
 function listBalances(sheet_id, _sheet_already_open) {
   var sheet = _sheet_already_open ?? openSheet(sheet_id);
   var balances = sheet.getSheetByName(BALANCES_SHEET);
@@ -89,12 +89,18 @@ function listBalances(sheet_id, _sheet_already_open) {
   if (!headers) throw new Error(BALANCES_SHEET + " does not contain a header row");
   var c_person = findColumn(headers, PERSON_COLUMN);
   var c_owed = findColumn(headers, OWED_COLUMN);
+  var users = listUsers(sheet_id);
   var result = {};
   for (var i = 1; i < values.length; i++) {
-    var row = values[i];
-    var email = row[c_person];
+    const row = values[i];
+    const email = row[c_person];
     if (email) {
-      result[email] = row[c_owed];
+      const alias = users[email];
+      if (alias) {
+        result[alias] = row[c_owed];
+      } else {
+        result[email] = row[c_owed];
+      }
     }
   }
   return {
