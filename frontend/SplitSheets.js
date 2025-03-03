ebb2a96802aa43dfcf03e8894fa69692e0925537
David Lang <davidlang42@gmail.com>
2025-03-03 20:27:32 +1100

    Avoid mutating response because it affects other things if the responses are chained

--- frontend/SplitSheets.js
+++ frontend/SplitSheets.js
@@ -217,7 +217,8 @@ function updateBalanceList(sheet_id, response, users) {
   document.getElementById("balance_list").innerHTML = new_list;
   document.getElementById("balance_last_updated").innerHTML = "Last updated: " + response.last_updated;
   //TODO remove listSheetsAndUsers because its too slow, also reduce 30s back to 10s
-  api.listSheets((sheets) => {
+  api.listSheets((original) => {//TODO a better way to make this safe is clone in the apiFrames combiner
+    let sheets = { ...original }; // clone before modifying
     delete sheets[sheet_id]; // dont offer to move to this sheet
     let sheets_and_users = {};
     for (const id in sheets) {
